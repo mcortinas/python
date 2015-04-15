c00ed3dfa9621da29ced02f4932ee1b4f98e1e83
Marc Cortinas <marc.cortinas@gamil.com>
2015-04-15 11:21:29 -0400

    adding project1

---
+++ project1/blog/models.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+from django.db import models
+from django.utils.timezone import utc
+import datetime
---
+++ project1/blog/views.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from django.shortcuts import render_to_response
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import RequestContext
+
+from blog import models
+
